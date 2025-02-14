4ab71abf6efaa9bf1b61bf01b0133f851b3a20aa
hieu29091996 <96782716+hieu29091996@users.noreply.github.com>
2025-02-14 14:54:01 +0700

    test

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+const CONFIGDATA = {
+  titleHeader: "Chào Trangg xinhh, Híu có điều muốn nói. </br> Làm người yêu Híuuu nhé kkk!",
+  descriptionHeader:
+    "Không ĐỒNG Ý cũng phải ĐỒNG Ý leuleu.",
+  buttonYes: "Tớ đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Cảm ơn em đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ em. </br>Đã lâu rồi anh muốn nói với em điều này: Anh thích em. Không phải thích bình thường, mà là thích rất nhèoo, yêu cực nhiều ♥️</br>
+
+Em có biết không, mỗi lần gặp em là tim anh lại đập nhanh hơn một nhịp. Valentine này, cho anh được nắm tay em nhé?"</br>  </br> Love you ♥️♥️♥️",
+};
